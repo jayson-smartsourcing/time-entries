c08599a2dd6e3dd478bd7f260226bc161aa9de56
Ernest Cesar Cueva <>
2019-06-07 13:54:34 +0800

    update

--- app/Http/Controllers/api/CameronFDController.php
+++ app/Http/Controllers/api/CameronFDController.php
@@ -422,7 +422,12 @@ class CameronFDController extends Controller
                         $group_name = $group->name;
                     }
 
-                    $hierarchy_id = $group_name.$value->custom_fields->cf_process.$value->custom_fields->cf_sub_process.$value->custom_fields->cf_task;
+                    $group_name = html_entity_decode($group_name);
+                    $process = html_entity_decode($value->custom_fields->cf_process);
+                    $sub_process = html_entity_decode($value->custom_fields->cf_sub_process);
+                    $task = html_entity_decode($value->custom_fields->cf_task);
+
+                    $hierarchy_id = $group_name.$process.$sub_process.$task;
                     if($value->type == "No SLA") {
                         $resolution_status = "Within SLA";
                     } else {
@@ -432,8 +437,7 @@ class CameronFDController extends Controller
                             $resolution_status = "SLA Violated";    
                         }
                     }
-                
-
+            
                     $agent_detail = $this->employee_ref->getEmployeeData($value->responder_id);
                     $date_executed = Carbon::parse($value->created_at)->format("Ymd");
                     $attendance_id = $date_executed.$agent_detail["SAL EMP ID"];
@@ -568,8 +572,6 @@ class CameronFDController extends Controller
                     $task = html_entity_decode($value->custom_fields->cf_task);
 
                     $hierarchy_id = $group_name.$process.$sub_process.$task;
-
-                    $hierarchy_id = $group_name.$value->custom_fields->cf_process.$value->custom_fields->cf_sub_process.$value->custom_fields->cf_task;
                     if($value->type == "No SLA") {
                         $resolution_status = "Within SLA";
                     } else {
@@ -589,9 +591,9 @@ class CameronFDController extends Controller
                         "hierarchy_id" => $hierarchy_id,
                         "resolution_status" => $resolution_status,
                         'type' => $value->type,
-                        'task' => $value->custom_fields->cf_task,
-                        'process' => $value->custom_fields->cf_process,
-                        'subprocess' => $value->custom_fields->cf_sub_process,
+                        'task' => $task,
+                        'process' => $process,
+                        'subprocess' => $sub_process,
                         'resolved_at' => Carbon::parse($value->stats->resolved_at)->setTimezone('Asia/Manila'),
                         'closed_at' => Carbon::parse($value->stats->closed_at)->setTimezone('Asia/Manila'),
                         "cc_emails" => json_encode($value->cc_emails),
