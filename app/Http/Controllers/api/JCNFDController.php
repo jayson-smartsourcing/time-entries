a96a1a2431563b02accc07940ce9fc64a471dfef
Ernest Cesar Cueva <>
2019-06-10 14:47:35 +0800

    update

--- app/Http/Controllers/api/JCNFDController.php
+++ app/Http/Controllers/api/JCNFDController.php
@@ -423,7 +423,12 @@ class JCNFDController extends Controller
                         $group_name = $group->name;
                     }
 
-                    $hierarchy_id = $group_name.$value->custom_fields->cf_process_beta.$value->custom_fields->cf_sub_process_beta.$value->custom_fields->cf_task_beta;
+                    $group_name = html_entity_decode($group_name);
+                    $process = html_entity_decode($value->custom_fields->cf_process_beta);
+                    $sub_process = html_entity_decode($value->custom_fields->cf_sub_process_beta);
+                    $task = html_entity_decode($value->custom_fields->cf_task_beta);
+
+                    $hierarchy_id = $group_name.$process.$sub_process.$task;
                     if($value->type == "No SLA") {
                         $resolution_status = "Within SLA";
                     } else {
@@ -444,9 +449,9 @@ class JCNFDController extends Controller
                         "hierarchy_id" => $hierarchy_id,
                         "resolution_status" => $resolution_status,
                         'type' => $value->type,
-                        'task' => $value->custom_fields->cf_task_beta,
-                        'process' => $value->custom_fields->cf_process_beta,
-                        'subprocess' => $value->custom_fields->cf_sub_process_beta,
+                        'task' => $task,
+                        'process' => $process,
+                        'subprocess' => $sub_process,
                         'resolved_at' => Carbon::parse($value->stats->resolved_at)->setTimezone('Asia/Manila'),
                         'closed_at' => Carbon::parse($value->stats->closed_at)->setTimezone('Asia/Manila'),
                         "cc_emails" => json_encode($value->cc_emails),
@@ -564,7 +569,12 @@ class JCNFDController extends Controller
                         $group_name = $group->name;
                     }
 
-                    $hierarchy_id = $group_name.$value->custom_fields->cf_process_beta.$value->custom_fields->cf_sub_process_beta.$value->custom_fields->cf_task_beta;
+                    $group_name = html_entity_decode($group_name);
+                    $process = html_entity_decode($value->custom_fields->cf_process_beta);
+                    $sub_process = html_entity_decode($value->custom_fields->cf_sub_process_beta);
+                    $task = html_entity_decode($value->custom_fields->cf_task_beta);
+
+                    $hierarchy_id = $group_name.$process.$sub_process.$task;
                     if($value->type == "No SLA") {
                         $resolution_status = "Within SLA";
                     } else {
@@ -585,9 +595,9 @@ class JCNFDController extends Controller
                         "hierarchy_id" => $hierarchy_id,
                         "resolution_status" => $resolution_status,
                         'type' => $value->type,
-                        'task' => $value->custom_fields->cf_task_beta,
-                        'process' => $value->custom_fields->cf_process_beta,
-                        'subprocess' => $value->custom_fields->cf_sub_process_beta,
+                        'task' => $task,
+                        'process' => $process,
+                        'subprocess' => $sub_process,
                         'resolved_at' => Carbon::parse($value->stats->resolved_at)->setTimezone('Asia/Manila'),
                         'closed_at' => Carbon::parse($value->stats->closed_at)->setTimezone('Asia/Manila'),
                         "cc_emails" => json_encode($value->cc_emails),
