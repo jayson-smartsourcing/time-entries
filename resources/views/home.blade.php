9b27225c7c852e29cafa25af9e11187ba694377a
Ernest Cesar Cueva <>
2019-06-26 09:28:12 +0800

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,6 +3,6 @@
 @section('content')
 <div class="container">
 <h4>EMPLOYEE NET SATISFACTION SCORE</h4>
-<iframe width="1400" height="800" src="https://app.powerbi.com/view?r=eyJrIjoiODZlNjQ0ZTktNWEzMi00YWNjLWFhMjktOTM5ZmQwNzhiNjEwIiwidCI6Ijg1MDAzZjczLWRlNGYtNGY1ZS04YzUxLWVkYzhiNjZmNjcwMyJ9" frameborder="0" allowFullScreen="true"></iframe>
+<iframe width="100%" height="800" src="https://app.powerbi.com/view?r=eyJrIjoiODZlNjQ0ZTktNWEzMi00YWNjLWFhMjktOTM5ZmQwNzhiNjEwIiwidCI6Ijg1MDAzZjczLWRlNGYtNGY1ZS04YzUxLWVkYzhiNjZmNjcwMyJ9" frameborder="0" allowFullScreen="true"></iframe>
 </div>
 @endsection
