9d6067faa1fced89fbffddc553b522bb8e83e2df
jayson-smartsourcing <48975639+jayson-smartsourcing@users.noreply.github.com>
2020-11-19 13:49:44 +0800

    Beer Cartel and Belle Property

---
+++ app/BellePropertyFDAgent.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+
+class BellePropertyFDAgent extends Model
+{
+    protected $table = 'belle_property_fd_agents';
+    protected $fillable = [
+       'id',
+       'first_name',
+       'last_name',
+       'created_at',
+       'updated_at'
+       
+    ];
+
+    public function bulkInsert($data){
+        return DB::table('belle_property_fd_agents')->insert($data);
+    }
+    //$ids_to_delete must be array
+    public function bulkDeleteAgentId($ids_to_delete){
+        return static::whereIn('id',$ids_to_delete)->delete();
+    }
+
+    public function getAgentById($id) {
+        return static::where('id',$id)->first();
+    }
+
+    public function truncateTable() {
+        return static::truncate();
+     }
+
+     public function deleteDuplicates($table_name) {
+        $values = [$table_name];
+        DB::insert('EXEC delete_duplicate ?', $values);
+    }
+}
---
+++ app/BellePropertyFDContact.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+
+class BellePropertyFDContact extends Model
+{
+    protected $table = 'belle_property_fd_contacts';
+    protected $fillable = [
+       'id',
+       'name',
+       'created_at',
+       'updated_at'
+       
+    ];
+    public function bulkInsert($data){
+        return DB::table('belle_property_fd_contacts')->insert($data);
+    }
+    //$ids_to_delete must be array
+    public function bulkDeleteAgentId($ids_to_delete){
+        return static::whereIn('id',$ids_to_delete)->delete();
+    }
+
+    public function getAgentById($id) {
+        return static::where('id',$id)->first();
+    }
+
+    public function truncateTable() {
+        return static::truncate();
+    }
+
+    public function addAgentsToContacts($table_name) {
+        $values = [$table_name];
+        DB::insert('EXEC insert_fd_agents_to_contacts ?', $values);
+    }
+
+    public function deleteDuplicates($table_name) {
+        $values = [$table_name];
+        DB::insert('EXEC delete_duplicate ?', $values);
+    }
+}
